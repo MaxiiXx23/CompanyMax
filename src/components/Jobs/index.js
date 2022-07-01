76eed623e44fdadebddadc064859d8b120ecffff
MaxiiXx23 <max.232017@gmail.com>
2022-06-30 22:51:56 -0300

    Lista de Jobs criados

--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -1,8 +1,22 @@
+import { FaPaperPlane } from 'react-icons/fa'
+import { Container, Content, Vacancy, Description, ButtonSend, List } from './styled'
 
-
-function Jobs(){
-    return(
-        <h1>Jobs Company</h1>
+function Jobs() {
+    return (
+        <Container>
+            <List>
+                <Content>
+                    <Vacancy>Programmer Junior</Vacancy>
+                    <Description>
+                        Junior programmer for creating websites in React, Node and Prisma.
+                    </Description>
+                    <ButtonSend>
+                        <FaPaperPlane size={24} />
+                        <span>Send résumé</span>
+                    </ButtonSend>
+                </Content>
+            </List>
+        </Container>
     )
 }
 
---
+++ src/components/Jobs/styled.js
@@ -0,0 +1,85 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    width: 80%;
+    display: block;
+    margin: 2px 0 2px;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`
+
+const Content = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row;
+    width: 100%;
+    padding: 10px;
+    border-radius: 2px;
+    margin: 2px 0 2px 0;
+    &:hover{
+        transition: 0.7s;
+        background: #8395a7;
+    }
+
+`
+const List = styled.nav`
+    display: block;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background: #222f3e;
+    color: #fff;
+    border: 1px solid #222f3e;
+    border-radius: 2px;
+    width: 100%;
+    height: 350px;
+    overflow-y: auto;
+    animation: balance 1s normal;
+    -webkit-animation: balance 1s normal;
+    @keyframes balance{
+        from {
+            transform: translateX(100px);
+            }
+        to {
+            transform: translateX(0px);
+        }
+    }
+    @-webkit-keyframes balance {
+        from {
+            transform: translateX(100px);
+        }
+        to {
+            transform: translateX(0px);
+        }
+    }
+    ul{
+        width: 100%;
+    }
+`
+
+const Vacancy = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+const Description = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+const ButtonSend = styled.button`
+    display: flex;
+    padding: 10px;
+    width: 13%;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    border: 1px solid #1dd1a1;
+    border-radius: 2px;
+    background: #1dd1a1;
+    cursor: pointer;
+`
+
+export { Container, Content, Vacancy, Description, ButtonSend, List }
